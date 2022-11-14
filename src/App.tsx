4f5918c1e1f0b188bb24ae8ca4f2d435c03dcd59
hblee12294 <hblee12294@gmail.com>
2022-11-14 11:33:57 -0800

    feat: setup @react-three

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Canvas } from '@react-three/fiber'
+
+import './App.css'
+import Box from './Box'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Canvas>
+      <color attach="background" args={['#fff']} />
+      <ambientLight intensity={0.5} />
+      <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
+      <pointLight position={[-10, -10, -10]} />
+
+      <Box position={[-1.2, 0, 0]} />
+      <Box position={[1.2, 0, 0]} />
+    </Canvas>
+  )
 }
 
-export default App;
+export default App
---
+++ src/Box.tsx
@@ -0,0 +1,34 @@
+import { useRef, useState } from 'react'
+import { MeshProps, useFrame } from '@react-three/fiber'
+import { Mesh } from 'three'
+
+function Box(props: MeshProps) {
+  const ref = useRef<Mesh>(null)
+
+  // Hold state for hovered and clicked events.
+  const [hovered, hover] = useState(false)
+  const [clicked, click] = useState(false)
+
+  // Subscribe this component to the render-loop and rotate the mesh every frame.
+  useFrame((state, delta) => {
+    if (ref.current) {
+      ref.current.rotation.x += delta
+    }
+  })
+
+  return (
+    <mesh
+      {...props}
+      ref={ref}
+      scale={clicked ? 1.5 : 1}
+      onClick={(event) => click(!clicked)}
+      onPointerOver={(event) => hover(true)}
+      onPointerOut={(event) => hover(false)}
+    >
+      <boxGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
+    </mesh>
+  )
+}
+
+export default Box
